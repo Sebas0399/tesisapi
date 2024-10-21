c4889a0ce417aa5ec26f74d71ec5a38c5e6f21ba
Sebas <antony.aguay@gmail.com>
2024-10-21 14:13:15 +0000

    fix

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from fastapi import FastAPI, File, UploadFile
 import numpy as np
 import tensorflow as tf
+import keras
+import keras_cv
+
 from PIL import Image
 from io import BytesIO
-import onnxruntime as rt
-from onnxruntime.datasets import get_example
-from onnx_tf.backend import prepare
 from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI()
 app.title="API de prueba"
 app.version="0.0.1"
 origins = ["*"]
-
+clases=["buena","mixta","mala"]
+model_aux=None
+model=None
 app.add_middleware(
     CORSMiddleware,
      allow_origins=origins,
@@ -20,46 +22,36 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-@app.post("/analyze/")
-async def analyze(file: UploadFile = File(...)):
+def select_model(model_id):
+    if model_id==1:
+        model = keras.saving.load_model("models/modelo_dark.keras")
+    elif model_id==2:
+        model = keras.saving.load_model("models/modelo_moblie.keras")
+    elif model_id==3:
+        model = keras.saving.load_model("models/modelo_resnet.keras")
+    return model
+@app.post("/analyze/{model_id:int}")
+async def analyze(model_id: int, file: UploadFile = File(...)):    
     image = await file.read()
-    return predecir(image)
+    return predecir(image,model_id)
 
-model = tf.keras.models.load_model("models/model.h5")
-sess = rt.InferenceSession('best.onnx', providers=rt.get_available_providers())
-clases=["buena","mixta","mala"]
-input_name = sess.get_inputs()[0].name
-print("input name", input_name)
-input_shape = sess.get_inputs()[0].shape
-print("input shape", input_shape)
-input_type = sess.get_inputs()[0].type
-print("input type", input_type)
+def predecir(imagen,model_id):
+    global model_aux, model
 
-output_name = sess.get_outputs()[0].name
-print("output name", output_name)
-output_shape = sess.get_outputs()[0].shape
-print("output shape", output_shape)
-output_type = sess.get_outputs()[0].type
-print("output type", output_type)
-def predecir(imagen):
+    if(model_aux!=model_id):
+        model_aux=model_id
+        model=select_model(model_id)
+    model = select_model(model_id)
     image = Image.open(BytesIO(imagen))
-    image = image.resize((224, 224))
+    image = image.resize((96, 96))
+    
     # Convertir la imagen a un array
-    img_array = tf.keras.preprocessing.image.img_to_array(image)
-    
-    # Añadir una dimensión extra
-    img_array = tf.expand_dims(img_array, 0)
+    input_arr = keras.utils.img_to_array(image)
+    input_arr = np.array([input_arr])  # Convert single image to a batch
 
-    # Predecir la clase de la imagen
-    #predictions = model.predict(img_array)
+    # Predict the class of the image
+    predictions = model.predict(input_arr)
+    print(predictions)
+    print(np.argmax(predictions[0]))
+    return f'Clase predicha: {clases[np.argmax(predictions[0])]}'
 
-    image = np.array(image).astype(np.float32) / 255.0
-    # Añadir dimensión de batch (1, altura, ancho, canales)
-    image = np.transpose(image, (2, 0, 1))
-    image = np.expand_dims(image, axis=0)
-   
-    res = sess.run([output_name], {input_name: image})
-    #predicted_class = tf.argmax(predictions[0]).numpy()
-    
-    return f'Clase predicha: {clases[np.argmax(res[0][0])]}'
